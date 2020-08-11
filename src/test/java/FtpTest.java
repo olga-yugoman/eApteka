7898d854907aa5e9ecb39166dd8cccaa236caa52
Ольга Андреевна Югоман <o.yugoman@eapteka.local>
2020-08-11 13:57:50 +0300

    added allure

--- src/test/java/FtpTest.java
+++ src/test/java/FtpTest.java
@@ -1,11 +1,15 @@
+import io.qameta.allure.Feature;
+import io.qameta.allure.Issue;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
 import java.io.File;
 
-public class TestBase {
+@Feature("Подготовка энва к тестированию.")
+public class FtpTest {
     private final FtpHelper ftp = new FtpHelper();;
 
+    @Issue("ONE-9904")
     @ParameterizedTest
     @CsvFileSource(resources = "/properties.csv", numLinesToSkip = 1)
     public void setUp(String host, String login, String password, String dir) throws Exception {
